82cd30cb4f222bfc0ac874ad357ebecd2096486e
Matt Simmons <simmonmt@acm.org>
1998-03-13 09:53:38 +0000

    Colin's change for migration error message

--- lisp/bbdb.el
+++ lisp/bbdb.el
@@ -37,6 +37,9 @@
 ;; $Id$
 ;;
 ;; $Log$
+;; Revision 1.61  1998/03/13 09:53:38  simmonmt
+;; Colin's change for migration error message
+;;
 ;; Revision 1.60  1998/03/10 07:45:44  simmonmt
 ;; Fixed defface standin.  Created widget (bbdb-alist-with-header) for
 ;; bbdb-auto-notes-alist customization fix.  Changed defaults for
@@ -89,7 +92,7 @@
 
 (require 'timezone)
 
-(defconst bbdb-version "1.90")
+(defconst bbdb-version "1.91")
 (defconst bbdb-version-date "$Date$")
 
 ;; File format
@@ -144,7 +147,7 @@ version.")
       nil)
     (defmacro defcustom (var value doc &rest args) 
       (` (defvar (, var) (, value) (, doc))))
-    (defmacro defface (face spec doc &rest args)
+    (defmacro defface (var value doc &rest args)
       (` (make-face (, var))))
     (defmacro define-widget (&rest args)
       nil)))
@@ -1548,7 +1551,7 @@ optional arg DONT-CHECK-DISK is non-nil (which is faster, but hazardous.)"
 		    (cons v (bbdb-migration-query v)))))
 	   ((> v bbdb-file-format)
 	    (error "BBDB version %s doesn't understand file format version %s."
-		   bbdb-version bbdb-file-format))
+		   bbdb-version v))
 	   (t
 	    (setq bbdb-file-format-migration (cons bbdb-file-format
 						   bbdb-file-format)))))
