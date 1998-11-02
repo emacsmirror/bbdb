4dc3380000592940a3d7394f4696af1bc91cc481
Matt Simmons <simmonmt@acm.org>
1998-11-02 07:07:50 +0000

    Change mailing list address

--- lisp/bbdb-print.el
+++ lisp/bbdb-print.el
@@ -56,6 +56,9 @@
 ;; $Id$
 ;;
 ;; $Log$
+;; Revision 1.59  1998/11/02 07:08:14  simmonmt
+;; Change mailing list address
+;;
 ;; Revision 1.58  1998/10/17 19:43:26  simmonmt
 ;; Patch to convert default area code protection from condition-case to
 ;; integerp.
@@ -93,7 +96,7 @@
 ;;; <boris@cs.rochester.edu> and Dirk Grunwald 
 ;;; <grunwald@cs.colorado.edu> using a TeX format designed by Luigi
 ;;; Semenzato <luigi@paris.cs.berkeley.edu>. 
-;;; We are also grateful to numerous people on the info-bbdb
+;;; We are also grateful to numerous people on the bbdb-info
 ;;; mailing list for suggestions and bug reports. 
 
 ;;; Code:
--- lisp/bbdb.el
+++ lisp/bbdb.el
@@ -19,8 +19,8 @@
 ;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 ;;;
 ;;;  ------------------------------------------------------------------------
-;;; |  There is a mailing list for discussion of BBDB: info-bbdb@xemacs.org  |
-;;; |  To join, send mail to info-bbdb-request@xemacs.org (don't forget the  |
+;;; |  There is a mailing list for discussion of BBDB: bbdb-info@xemacs.org  |
+;;; |  To join, send mail to bbdb-info-request@xemacs.org (don't forget the  |
 ;;; |  -request part or you'll look silly in front of lots of people who     |
 ;;; |  have the ability to remember it indefinitely...)                      |
 ;;; |                                                                        |
@@ -37,6 +37,9 @@
 ;; $Id$
 ;;
 ;; $Log$
+;; Revision 1.65  1998/11/02 07:07:50  simmonmt
+;; Change mailing list address
+;;
 ;; Revision 1.64  1998/10/10 18:49:27  simmonmt
 ;; From slbaur - Switch from `screen' functions to `frame' functions
 ;;
@@ -105,7 +108,7 @@
 
 (require 'timezone)
 
-(defconst bbdb-version "2.00.02")
+(defconst bbdb-version "2.00.03")
 (defconst bbdb-version-date "$Date$")
 
 ;; File format
