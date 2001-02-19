984d27cfbfd27f72fd755d6806391232f2158917
Ronan Waide <waider@waider.ie>
2001-02-19 23:10:27 +0000

    Now that I've fixed bbdb-gui properly, reenable the load hook.

--- lisp/bbdb.el
+++ lisp/bbdb.el
@@ -3118,8 +3118,8 @@ passed as arguments to initiate the appropriate insinuations.
 
 ;; Hook in GUI hacks
 ;; Do not simply require this, as it needs bbdb.el to be loaded first.
-;(or (eq window-system nil)
-;    (add-hook 'bbdb-load-hook '(lambda()(require 'bbdb-gui))))
+(or (eq window-system nil)
+    (add-hook 'bbdb-load-hook '(lambda()(require 'bbdb-gui))))
 
 (provide 'bbdb)  ; provide before loading things which might require
 
