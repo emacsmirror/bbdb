0325f18cca9e34fb983c24c6cda24c7c16686c95
Robert Fenk <fenk@users.sourceforge.net>
2003-03-27 21:18:37 +0000

    *** empty log message ***

--- lisp/bbdb-com.el
+++ lisp/bbdb-com.el
@@ -2727,7 +2727,8 @@ of all of those people."
   (mail-abbrev-expand-hook)
 
   (when bbdb-completion-display-record
-    (bbdb-pop-up-bbdb-buffer bbdb-use-pop-up)
+    (if bbdb-use-pop-up
+        (bbdb-pop-up-bbdb-buffer))
     (let ((bbdb-gag-messages t))
       (bbdb-display-records-1
        (mapcar (lambda (x) (bbdb-search-simple nil x)) records)
