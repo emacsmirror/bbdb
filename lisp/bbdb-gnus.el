1ec55cf9e26866bd410d40a70c9b0e82f32209a7
Robert Fenk <fenk@users.sourceforge.net>
2003-10-13 08:34:49 +0000

    (bbdb/gnus-split-method): Honor Resent-* headers if present. (Reported by Thomas Gerds)

--- lisp/bbdb-gnus.el
+++ lisp/bbdb-gnus.el
@@ -628,13 +628,19 @@ spooled, using the addresses in the headers and information from the
 BBDB."
   (let ((prq (list (cons 0 nil) (cons 1 nil) (cons 2 nil) (cons 3 nil))))
     ;; the From: header is special
-    (let* ((hdr (or (mail-fetch-field "from") (user-login-name)))
+    (let* ((hdr (or (mail-fetch-field "resent-from")
+                    (mail-fetch-field "from")
+                    (user-login-name)))
        (rv (bbdb/gnus-split-to-group hdr t)))
       (setcdr (nth (cdr rv) prq) (cons (car rv) nil)))
     ;; do the rest of the headers
-    (let ((hdr (or (concat (mail-fetch-field "to" nil t) ", "
-               (mail-fetch-field "cc" nil t) ", "
-               (mail-fetch-field "apparently-to" nil t)) "")))
+    (let ((hdr (or (concat (or (mail-fetch-field "resent-to" nil t)
+                               (mail-fetch-field "to" nil t))
+                           ", "
+                           (mail-fetch-field "cc" nil t)
+                           ", "
+                           (mail-fetch-field "apparently-to" nil t))
+                   "")))
       (setq hdr (rfc822-addresses hdr))
       (while hdr
     (let* ((rv (bbdb/gnus-split-to-group (car hdr)))
