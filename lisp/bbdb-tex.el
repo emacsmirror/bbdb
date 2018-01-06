307bad59bc3debf250a8617ab699b50a9402c0ae
Roland Winkler <winkler@gnu.org>
2018-01-06 22:40:02 +0530

    bbdb-tex-alist, bbdb-tex-address-layout: Declare defcustom type.

--- lisp/bbdb-tex.el
+++ lisp/bbdb-tex.el
@@ -241,7 +241,15 @@ The remainder are lists LIST that should have one of these forms:
  (epilog STRING)
 
   The string STRING is inserted at the end of the buffer."
-  :group 'bbdb-utilities-tex)
+  :group 'bbdb-utilities-TeX
+  :type '(repeat (cons (symbol :tag "rule")
+                       (repeat
+                        (choice (cons :tag "demand" (const demand) sexp)
+                                (list :tag "prolog" (const prolog) string)
+                                (cons :tag "record" (const record) sexp)
+                                (list :tag "separator" (const separator) string)
+                                (list :tag "epilog" (const epilog) string)
+                                (cons :tag "options" (const options) sexp))))))
 
 (defcustom bbdb-tex-rule-default 'multi-line
   "Default rule for BBDB tex.
@@ -289,7 +297,9 @@ The elements EDIT of `bbdb-address-format-list' are ignored."
 (defcustom bbdb-tex-address-layout 2
   "Address layout according to `bbdb-tex-address-format-list'.
 2 is multi-line layout, 3 is one-line layout."
-  :group 'bbdb-utilities-tex)
+  :group 'bbdb-utilities-TeX
+  :type '(choice (const :tag "multi-line" 2)
+                 (const :tag "one-line" 3)))
 
 (defcustom bbdb-tex-file "~/bbdb.tex"
   "Default file name for TeXing BBDB."
